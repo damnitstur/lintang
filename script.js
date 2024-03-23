c1e1d5a154e3597eed1d49c43be9eab48e45a918
damnitstur <164265621+damnitstur@users.noreply.github.com>
2024-03-24 01:25:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function home() {
-    $('#welcomeText').text('Selamat datang di website resmi Lintang Family').show(); // Menampilkan kembali #welcomeText
+    $('#welcomeText').text('Selamat datang di website resmi Lintang Family').show(); 
     $('.gallery-container').hide();
     $('.content audio').remove();
     $('.content p').remove();
-    $('#aboutDescription').hide(); // Sembunyikan #aboutDescription di halaman home
-    $('#anggotaFamily').hide(); // Sembunyikan #anggotaFamily di halaman home
+    $('#aboutDescription').hide();
+    $('#anggotaFamily').hide();
 }
 
 function about() {
@@ -90,8 +90,10 @@ function addMedia() {
     $('#aboutDescription').hide();
     $('#anggotaFamily').hide(); 
 
-    var audio = document.createElement("audio");
-    audio.controls = true;
-    audio.src = "https://drive.google.com/uc?export=download&id=1koF_3qQH-SWPNmSDS-PUgCDnwg4Am08E";
-    document.querySelector('.content').appendChild(audio);
+
+    var div = document.createElement("div");
+    div.innerHTML = '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1782701706&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/its-tur" title="It&#x27;s Tur" target="_blank" style="color: #cccccc; text-decoration: none;">It&#x27;s Tur</a> · <a href="https://soundcloud.com/its-tur/l-lintang" title="L~ Lintang" target="_blank" style="color: #cccccc; text-decoration: none;">L~ Lintang</a></div>';
+    
+    document.querySelector('.content').appendChild(div);
 }
+
